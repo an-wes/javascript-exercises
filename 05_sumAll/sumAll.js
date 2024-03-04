e1fb17bfd041ea218aa8ca5098d223ccb75e1d81
Andreas Westergren <anwes@kth.se>
2024-03-04 21:09:05 +0100

    Pass 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number')
+   return 'ERROR'
+  
+  const n = Math.abs(a - b) + 1;
+  return n * (a + b) / 2;
 };
 
 // Do not edit below this line
