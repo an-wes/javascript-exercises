869d45a30aa43c669ff0acfb11667c345a96984f
Andreas Westergren <anwes@kth.se>
2024-03-04 21:19:25 +0100

    Pass 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  const tempInCelsius = (tempInFarenheit - 32) * (5 / 9);
+  return Math.round(tempInCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const tempInFarenheit = tempInCelsius * (9 / 5) + 32;
+  return Math.round(tempInFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
