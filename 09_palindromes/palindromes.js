013367daead873b8cde7b9bdb81a8be0e9e9059d
Andreas Westergren <anwes@kth.se>
2024-03-09 16:38:34 +0100

    Pass 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (s) {
+  const cleanString = s
+    .toLowerCase()
+    .split('')
+    .filter(x => isAlphaNumerical(x))
+    .join('');
+    
+    const reversed = cleanString.split('').reverse().join('');
 
+  return cleanString === reversed;
 };
+
+const isAlphaNumerical = function (c) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  return alphanumerical.includes(c);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
