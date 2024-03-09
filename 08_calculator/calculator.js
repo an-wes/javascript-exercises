6127029c2c4a67b17bb3dea6e1c1b09603dda6fd
Andreas Westergren <anwes@kth.se>
2024-03-09 16:17:47 +0100

    Pass 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return add(a, -b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let res = 0;
+  for (let element of arr) {
+    res = add(res, element);
+  }
+	return res;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  for (let element of arr) {
+    sum *= element;
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	return Math.pow(base, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
