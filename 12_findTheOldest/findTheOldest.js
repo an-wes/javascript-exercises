56f0c1e9d79eb66b6a1c15caa0972f8d488170b9
Andreas Westergren <anwes@kth.se>
2024-04-04 12:49:11 +0200

    Finalized exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const arr = people
+    .map(p => ({ person: p, age: getAge(p) }));
 
+  arr.sort((a,b) => b.age - a.age);
+  return arr[0].person;
 };
+
+const getAge = function(person) {
+  const currentYear = new Date().getFullYear();
+  return (person.yearOfDeath === undefined ? currentYear : person.yearOfDeath) - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
